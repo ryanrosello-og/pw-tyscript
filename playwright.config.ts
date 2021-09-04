748c6577cabe0f7a86ec39bee6d4a1a112d49349
ryanrosello-og <goneby30@gmail.com>
2021-09-04 16:24:10 +1000

    typsescript e.g.

---
+++ playwright.config.ts
@@ -0,0 +1,9 @@
+// playwright.config.ts
+import { PlaywrightTestConfig } from '@playwright/test';
+
+const config: PlaywrightTestConfig = {
+  use: {
+    baseURL: 'https://playwright.dev/',
+  },
+};
+export default config;
---
+++ tests/foo.spec.ts
@@ -0,0 +1,30 @@
+import { test, expect } from '@playwright/test';
+import { DevPage } from '../pages/devPage';
+
+test('basic test', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+  const title = page.locator('.navbar__inner .navbar__title');
+  await expect(title).toHaveText('Playwright');
+});
+
+test('dev Page', async ({ page }) => {
+  const pwDevPage = new DevPage(page);
+  await pwDevPage.goto();
+  await pwDevPage.clickGetStarted();
+  // expect(await pwDevPage.getToc()).toEqual([
+  //   'Installation',
+  //   'Usage',
+  //   'First script',
+  //   'Record scripts',
+  //   'TypeScript support',
+  //   'System requirements',
+  //   'Release notes',
+  // ]);
+});
+
+test('Core Concepts table of contents', async ({ page }) => {
+  const playwrightDev = new DevPage(page);
+  await playwrightDev.goto();
+  // await playwrightDev.clickTradeOffs();
+  //expect(await playwrightDev.whyPwIsShown()).toBeTruthy();
+});

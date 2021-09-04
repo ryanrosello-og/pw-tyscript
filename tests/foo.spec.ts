c05660f2eaec2d82044612ab54e8c67cb5d868b4
ryanrosello-og <goneby30@gmail.com>
2021-09-04 16:29:52 +1000

    nfi

--- tests/foo.spec.ts
+++ tests/foo.spec.ts
@@ -1,8 +1,12 @@
 import { test, expect } from '@playwright/test';
 import { DevPage } from '../pages/devPage';
 
-test('basic test', async ({ page }) => {
+test.beforeEach(async ({ page }) => {
   await page.goto('https://playwright.dev/');
+});
+
+test('basic test', async ({ page }) => {
+  // await page.goto('https://playwright.dev/');
   const title = page.locator('.navbar__inner .navbar__title');
   await expect(title).toHaveText('Playwright');
 });
@@ -23,8 +27,11 @@ test('dev Page', async ({ page }) => {
 });
 
 test('Core Concepts table of contents', async ({ page }) => {
-  const playwrightDev = new DevPage(page);
-  await playwrightDev.goto();
+  // const playwrightDev = new DevPage(page);
+  // await playwrightDev.goto();
+
+  const pwDevPage = new DevPage(page);
+  await pwDevPage.goto();
   // await playwrightDev.clickTradeOffs();
   //expect(await playwrightDev.whyPwIsShown()).toBeTruthy();
 });
